28658b2727c8feeef672c1c3f197a030d1805472
oy6uns <bacho4235@gmail.com>
2023-05-14 23:56:34 +0900

    [Feat] AWS S3 bucket에서 이미지 불러온뒤, 합치는 코드 구현

--- predict_breed.py
+++ predict_breed.py
@@ -47,7 +47,6 @@ async def predict(file: UploadFile = File(...)):
 model = torch.load('resnet50_fintuned_epoch50_v1.pt', map_location=torch.device('cpu'))
 
 # need to get the ids from the sample_submission csv so we can match it up 
-# need to get the ids from the sample_submission csv so we can match it up 
 labels = dict()
 for index, value in enumerate(pd.read_csv('dog-breed-identification/sample_submission.csv').columns[1:]):
     labels[value] = index
@@ -82,16 +81,79 @@ async def get_images_from_s3(texts: list[str]):
     s3 = boto3.client('s3')
 
     def generate_images():
+        images = []
         for text in texts:
             try:
                 response = s3.get_object(Bucket='dog-icon-component-bucket', Key=f'{text}.png')
-                yield response['Body'].read()
+                image_data = response['Body'].read()
+                image = Image.open(io.BytesIO(image_data))
+                images.append(image)
             except s3.exceptions.NoSuchKey:
                 raise HTTPException(status_code=404, detail=f"No image found for text: {text}")
+        return images
+    
+    images = generate_images()
 
-    return StreamingResponse(generate_images(), media_type="image/png")
+    if len(images) < len(texts):
+        raise HTTPException(status_code=500, detail="Some images could not be retrieved.")
 
+    ear_image = images[0]
+    fur_image = images[1]
+    pattern_image = images[2]
 
+    def make_ear_position(ear_image, fur_image):
+        ear_x1 = int((fur_image.size[0] - ear_image.size[0]) / 2)
+        ear_x2 = fur_image.size[0] - ear_x1
+        ear_y1 = 50
+        ear_y2 = ear_y1 + ear_image.size[1]
+
+        area = (ear_x1, ear_y1, ear_x2, ear_y2)
+        return area
+
+    def make_pattern_position(pattern_image, fur_image):
+        x1 = int((fur_image.size[0] - pattern_image.size[0]) / 2)
+        x2 = pattern_image.size[0] + x1
+        y1 = int((fur_image.size[1] - pattern_image.size[1]) / 2)
+        y2 = pattern_image.size[1] + y1
+
+        area = (x1, y1, x2, y2)
+        return area
+
+    def make_color_transparent(image, target_color):
+        # 이미지에 알파 채널(투명도) 추가
+        image = image.convert("RGBA")
+
+        # 이미지의 픽셀 데이터 가져오기
+        data = image.getdata()
+
+        # 새로운 픽셀 데이터 생성
+        new_data = []
+        for item in data:
+            # 대상 색상과 일치하는 경우 알파 값을 0으로 설정하여 투명하게 만듦
+            if item[:3] == target_color:
+                new_data.append((*target_color, 0))
+            else:
+               new_data.append(item)
+
+        # 이미지에 새로운 픽셀 데이터 적용
+        image.putdata(new_data)
+
+    # 대상 이외의 배경을 제거하는 함수
+    make_color_transparent(ear_image, (255, 255, 255))
+    make_color_transparent(pattern_image, (255, 255, 255))
+
+    area_ear = make_ear_position(ear_image, fur_image)
+    area_pattern = make_pattern_position(pattern_image, fur_image)
+    print(area_pattern)
+
+    fur_image.paste(ear_image, area_ear, mask=ear_image)
+    fur_image.paste(pattern_image, area_pattern, mask=pattern_image)
+
+    image_bytes = io.BytesIO()
+    fur_image.save(image_bytes, format='PNG')
+    image_bytes.seek(0)
+
+    return StreamingResponse(image_bytes, media_type="image/png")
 
 if __name__ == '__main__':
     uvicorn.run(app, host="127.0.0.1", port = 8000)
